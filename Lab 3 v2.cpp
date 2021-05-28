5567661ff74fe7f566ad382ef43789288bd8ca5b
Artem Samusev <samusev.a@live.unic.ac.cy>
2021-05-28 09:01:50 +0300

    Add functions

--- Lab 3 v2.cpp
+++ Lab 3 v2.cpp
@@ -2,8 +2,13 @@
 //Author:Artem Samusev
 
 #include <iostream>
+#include <cassert>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+int printMenu();
+void fillinarr(int[], const int);
+void multarr(const int[], const int[], int[], const int);
+void displayarr(const int[], const int);
+int sumodd(const int[], const int);
+bool allisposit(const int[], const int);
+void averageodd(const int[], const int, int&);
