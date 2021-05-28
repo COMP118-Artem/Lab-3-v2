26a288a1694a8dd7793ede39c5afd9d638e72c83
Artem Samusev <samusev.a@live.unic.ac.cy>
2021-05-28 09:01:01 +0300

    Add project files.

---
+++ Lab 3 v2.cpp
@@ -0,0 +1,9 @@
+// Lab 3 v2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Author:Artem Samusev
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
